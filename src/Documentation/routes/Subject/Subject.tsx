9b4724bcaf51aba03705e11bd6dfc160a25e89f7
Antoine Regembal <antoine.regembal@axa.fr>
2022-06-23 14:41:28 +0200

    feat(Documentation): Updating documentation style.

--- src/Documentation/routes/Subject/Subject.tsx
+++ src/Documentation/routes/Subject/Subject.tsx
@@ -1,7 +1,7 @@
 export const Subject = () => (
 	<>
 		<section>
-			<h2>Subject</h2>
+			<h2 className="section__title--subject">Subject</h2>
 			<section>
 				<h3>Existing and goal</h3>
 				<p>You have been given a simple react form. The form is entirely functional with state management, form validation and errors management.</p>
@@ -27,17 +27,19 @@ export const Subject = () => (
 			</section>
 		</section>
 		<section>
-			<h2>How to validate that my refactoring is correct ?</h2>
+			<h2 className="section__title--refactor-validation">How to validate that my refactoring is correct ?</h2>
 			<section>
 				<h3>Use existing tests</h3>
 				<p>The project comes with a full set of tests covering the different use cases of the form.</p>
 				<p>To validate that your refactoring is correct, run the different tests suites with the command below.</p>
 				<p>If after your refactoring all the test are passing, then you can consider that your refactoring is working.</p>
-				<p className="snippet">npm run test</p>
+				<div className="snippet" onClick={() => {navigator.clipboard.writeText("npm run test");}}>
+					<p>npm run test</p>
+				</div>
 			</section>
 		</section>
 		<section>
-			<h2>Tips</h2>
+			<h2 className="section__title--tips">Tips</h2>
 			<p>Here is a list of tips that can help you during all the refactoring process :</p>
 			<ul>
 				<li>Launch tests suites during all the refactoring process to check what is working and what is not</li>
