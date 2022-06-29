80e03147ad370defdbce864954056e2e9b2835e7
Antoine Regembal <antoine.regembal@axa.fr>
2022-06-29 17:40:51 +0200

    feat(Help): Completing Help page with tips and infos.

--- src/Documentation/routes/Help/Help.tsx
+++ src/Documentation/routes/Help/Help.tsx
@@ -1,1 +1,61 @@
-export const Help = () => <p>it works</p>;
+export const Help = () => (
+	<>
+		<section>
+			<h2 className="section__title--refactor">
+				How do I refactor this application ?
+			</h2>
+			<section>
+				<h3 className="section__title--tips">Tips</h3>
+				<p className="block tips">
+					Their are several comments in the original code that will
+					guide you to start the refactoring process.
+				</p>
+			</section>
+			<section>
+				<h3>Step by Step</h3>
+				<p>
+					Do not change too much things at once if you are not
+					comfortable with React, that could lead to a really hard
+					time to make tests passing again.
+				</p>
+				<p>
+					If you are really stuck in the refactoring process, these
+					steps should help you be back on the track :
+				</p>
+				<ul className="block">
+					<li>Import useForm hook into your component file</li>
+					<li>
+						Create an instance of the useForm hook inside your
+						component and get the functions from it that will allow
+						you to manage fields (like register). Check the
+						documentation to see which parameters you could give to
+						useForm to get the desired behavior from it 😉
+					</li>
+					<li>
+						Register the fields into react-hook-form. Also if
+						needed, define validation requirements for each fields
+						with generic or custom rules and messages
+					</li>
+					<li>
+						Remove legacy fields controls like onChange, onBlur,
+						value. By default, react-hook-form do that for you
+						(depending of the configuration you give to the useForm
+						hook)
+					</li>
+					<li>
+						When form is submited by clicking on the Submit button,
+						make react-hook-form handle submition by getting the right function from useForm
+					</li>
+					<li>
+						Get the formState from useForm and manage to display fields errors below them
+					</li>
+				</ul>
+				<p>
+					When the refactoring process is done, the useReducer originaly used for form
+					state and errors management becomes useless and can be deleted.
+					It is the same thing for the validateForm and validateFieldRules functions.
+				</p>
+			</section>
+		</section>
+	</>
+);
--- src/Documentation/routes/Subject/Subject.tsx
+++ src/Documentation/routes/Subject/Subject.tsx
@@ -4,50 +4,107 @@ export const Subject = () => (
 			<h2 className="section__title--subject">Subject</h2>
 			<section>
 				<h3>Existing and goal</h3>
-				<p>You have been given a simple react form. The form is entirely functional with state management, form validation and errors management.</p>
 				<p>
-                    This form does not initially includes react-hook-form.
-                    Your goal is to refactor the form to include react-hook-form for state management, form validation and errors management.
+					You have been given a simple react form. The form is
+					entirely functional with state management, form validation
+					and errors management.
+				</p>
+				<p>
+					This form does not initially includes react-hook-form. Your
+					goal is to refactor the form to include react-hook-form for
+					state management, form validation and errors management.
 				</p>
 			</section>
 			<section>
 				<h3>Optional challenges</h3>
-				<p>If you want to, you can complete the initial goal with these optional ones :</p>
-				<ul>
-					<li>Persist the form state in the sessionStorage and make sure form data are retrieved and processed by react-hook-form when reloading the application page</li>
+				<p>
+					If you want to, you can complete the initial goal with these
+					optional ones :
+				</p>
+				<ul className="block">
 					<li>
-                        Adding a{" "}
-						<a href="https://react-hook-form.com/api/useform/#validationResolver" target="_blank" rel="noreferrer">resolver</a>{" "}
-                        for the errors management with react-hook-form. With{" "}
-						<a href="https://www.npmjs.com/package/yup" target="_blank" rel="noreferrer">Yup</a>{" "}
-                        for example
+						Persist the form state in the sessionStorage and make
+						sure form data are retrieved and processed by
+						react-hook-form when reloading the application page
 					</li>
-					<li>Adding more fields with interaction between fields, like a checkbox that display and hide a field when clicking on it (and see how react-hook-form manage hidden fields 🤔)</li>
+					<li>
+						Adding a{" "}
+						<a
+							href="https://react-hook-form.com/api/useform/#validationResolver"
+							target="_blank"
+							rel="noreferrer"
+						>
+							resolver
+						</a>{" "}
+						for the errors management with react-hook-form. With{" "}
+						<a
+							href="https://www.npmjs.com/package/yup"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Yup
+						</a>{" "}
+						for example
+					</li>
+					<li>
+						Adding more fields with interaction between fields, like
+						a checkbox that display and hide a field when clicking
+						on it (and see how react-hook-form manage hidden fields
+						🤔)
+					</li>
 				</ul>
 			</section>
 		</section>
 		<section>
-			<h2 className="section__title--refactor-validation">How to validate that my refactoring is correct ?</h2>
+			<h2 className="section__title--refactor-validation">
+				How to validate that my refactoring is correct ?
+			</h2>
 			<section>
 				<h3>Use existing tests</h3>
-				<p>The project comes with a full set of tests covering the different use cases of the form.</p>
-				<p>To validate that your refactoring is correct, run the different tests suites with the command below.</p>
-				<p>If after your refactoring all the test are passing, then you can consider that your refactoring is working.</p>
-				<div className="snippet" onClick={() => {navigator.clipboard.writeText("npm run test");}}>
+				<p>
+					The project comes with a full set of tests covering the
+					different use cases of the form.
+				</p>
+				<p>
+					To validate that your refactoring is correct, run the
+					different tests suites with the command below.
+				</p>
+				<p>
+					If after your refactoring all the test are passing, then you
+					can consider that your refactoring is working.
+				</p>
+				<div
+					className="snippet"
+					onClick={() => {
+						navigator.clipboard.writeText("npm run test");
+					}}
+				>
 					<p>npm run test</p>
 				</div>
 			</section>
 		</section>
 		<section>
 			<h2 className="section__title--tips">Tips</h2>
-			<ul className="tips">
-				<li>Launch tests suites during all the refactoring process to check what is working and what is not</li>
-				<li>Try to not change everything at once. That could result on a really hard time to make test passing again</li>
+			<ul className="block tips">
 				<li>
-					Do not modify existing tests, they make sure the form is working and confirm if your refactoring is working too.
-					However if you are adding more fields for the opitonal challenges make sure to add needed tests to cover the new cases
+					Launch tests suites during all the refactoring process to
+					check what is working and what is not
 				</li>
-				<li>Use the React Developer Tool to see what is happening inside your form while refactoring</li>
+				<li>
+					Try to not change everything at once. That could result on a
+					really hard time to make test passing again
+				</li>
+				<li>
+					Do not modify existing tests, they make sure the form is
+					working and confirm if your refactoring is working too.
+					However if you are adding more fields for the opitonal
+					challenges make sure to add needed tests to cover the new
+					cases
+				</li>
+				<li>
+					Use the React Developer Tool to see what is happening inside
+					your form while refactoring
+				</li>
 				<li>Read the documentation</li>
 			</ul>
 		</section>
